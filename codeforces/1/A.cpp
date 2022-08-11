03232867b01762f898279d3b2da5982efac30071
Vishavjeet Ghotra <vishavghotra11@gmail.com>
2022-08-11 20:36:00 +0200

    Add solution for problem `A - Theatre Square` Link: https://codeforces.com/contest/1/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1/submission/167903774

---
+++ codeforces/1/A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, a, b, c, j, k, l;
+int main()
+{
+    long m, n, a;
+    cin >> m >> n >> a;
+    cout << (((m + a - 1LL) / a) * ((n + a - 1LL) / a));
+}
